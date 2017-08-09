dedcb9c21b1877802f1610a543edea6a906287dc
ashenbrownfox <newhoopsdynasty@gmail.com>
2017-08-09 14:47:38 -0400

    First commit, settting up initial project.

---
+++ AdvRPG/AdvRPG.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvRPG
+{
+    public partial class AdvRPG : Form
+    {
+        public AdvRPG()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Hey I changedz!";
+            lblExperience.Text = "230000";
+        }
+
+        private void AdvRPG_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
